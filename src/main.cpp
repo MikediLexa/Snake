7f147c39eb905190bb146d418cdc6236652914c0
mikedilexa <mi.schneider.90@gmail.com>
2025-12-17 22:50:39 +0100

    main.cpp auf Init-Code umgebaut

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
-/*
- * main.cpp
- *
- *  Created on: 13.12.2025
- *      Author: mike
- */
-
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
-// #include <SDL3_image/SDL_image.h>
-// #include <SDL3_ttf/SDL_ttf.h>
+#include <SDL3_image/SDL_image.h>
+#include <SDL3_ttf/SDL_ttf.h>
 
 int main(int, char**) {
   SDL_Init(SDL_INIT_VIDEO);
@@ -32,32 +25,3 @@ int main(int, char**) {
 
   return 0;
 }
-
-//#include <iostream>
-//
-//
-//#include "config.h"
-//
-//
-//#include"Game/Game.h"
-//#include "gui/Window.h"
-//
-//
-//int main(int argc, char **argv)
-//{
-//	std::cout << "This is Snake" << std::endl;
-//	std::cout << "Version " << Snake_VERSION_MAJOR << "." << Snake_VERSION_MINOR << std::endl;
-//
-////	Game game;
-////	game.start();
-//	Window window;
-//	window.create(800, 100);
-//	std::cout << "Something should have happened!" << std::endl;
-//
-//	Game newGame = Game();
-//
-//
-//
-//	return 0;
-//}
-
